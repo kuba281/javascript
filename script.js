c9904aad2ca94b2fed33dd3a90f0b17bf3755048
JakubIzd <jakubizdebski65@gmail.com>
2026-01-26 19:31:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log('Skrypt załadowany!');
+
+const imie = "Adam";
+const wiek = 25;
+const czyStudent = true;
+const jezyki = ["JS", "Python", "C#"];
+const osoba = {
+    imie: "Adam",
+    wiek: 25,
+    miasto: "Warszawa"
+};
+const pustaWartosc = null;
+let nieokreslone;
+
+console.log(typeof imie);
+console.log(typeof wiek);
+console.log(typeof czyStudent);
+console.log(typeof jezyki);
+console.log(typeof osoba);
+console.log(typeof pustaWartosc);
+console.log(typeof nieokreslone);
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+console.log('5' == 5);
+console.log('5' === 5);
+
+let x = true;
+let y = false;
+
+console.log(x && y);
+console.log(x || y);
+console.log(!x);
